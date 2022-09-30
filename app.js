4024b6454681c46cd5ef94f8d7e03b5eb86bab84
Vic <vicjs612@gmail.com>
2022-09-29 21:00:26 -0500

    todo-filter branch

--- app.js
+++ app.js
@@ -38,21 +38,51 @@
     
     const populateTodos = () => {
       console.log('arrayOfTodos In POPULATE:', arrayOfTodos)
+      let newList = document.getElementById('todo-list')
       for (let i = 0; i < arrayOfTodos.length; i++) {
         const todo = arrayOfTodos[i];
         console.log('todo:', todo)
         console.log('todo["title"]:', todo['title'])
       
-      let newList = document.getElementById('todo-list')
       let userTitle = document.createElement('LI')
       userTitle.innerHTML = todo.title
       newList.appendChild(userTitle)
       
-      let idInsert = document.createTextNode(todo.id)
-      let userId = document.createTextNode(todo.userId)
-      let completed = document.createTextNode(todo.completed)
-      newList.append("User ID: ", userId, "ID: ", idInsert, "Completed? ", completed)
+    //   let idInsert = document.createTextNode(todo.id)
+    //   let userId = document.createTextNode(todo.userId)
+    //   let completed = document.createTextNode(todo.completed)
+    //   newList.append("User ID: ", userId, "ID: ", idInsert, "Completed? ", completed)
          
       
       }
+    }
+    const filterTodos = () => {
+        console.log('filterTodos')
+        // clear todos/element on page
+        let newList = document.getElementById('todo-list').innerHTML = ""
+        // get input elment byID
+        let inputElement = document.getElementById('id-input')
+        // get input value
+        let inputValue = document.querySelector('input').value;
+        console.log(inputValue);
+        // filter thru array of todos to find userId === input.value
+        // get ol parent element
+        // create list item
+        // create textNode to List item
+        // append list item to parent element
+        const filterArrayOfTodos = arrayOfTodos.filter((arrayOfTodos) =>{
+            if (arrayOfTodos.userId > 1 && arrayOfTodos.userId < 11){
+                return arrayOfTodos.userId
+            }
+        })
+        console.log('filterArrayOfTodos:', filterArrayOfTodos)
+        for (let i = 0; i < arrayOfTodos.length; i++) {
+            const todo = arrayOfTodos[i];
+            console.log('todo.uderId:', todo.userId)
+            let userId = document.createElement('LI')
+            userId.innerHTML = todo.userId
+            inputElement.appendChild(userId)
+        }
+        
+  
     }
